99315bac7f787f301f8c1213b846d47f6b468435
Mohamed Fares Ben Ayed <faresaction@yahoo.fr>
2024-12-18 20:56:27 +0100

    add order list

--- application/routes.go
+++ application/routes.go
@@ -4,21 +4,26 @@ import (
 	"net/http"
 
 	"github.com/BenAyedSeeksAI/micro-serv-go/handler"
+	"github.com/BenAyedSeeksAI/micro-serv-go/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 	router.Post("/create", orderHandler.Create)
 	router.Get("/list", orderHandler.List)
 	router.Get("/get/{id}", orderHandler.GetByID)
